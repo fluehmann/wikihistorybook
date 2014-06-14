998e19db1e902cecea9cfc6dc5c056514f4d1d2f
delta99 <silvioPi>
2014-06-14 14:13:36 +0200

    2nd version of wikibook

---
+++ wikihistorybook/wikiBook2/CloseWindowListener.java
@@ -0,0 +1,18 @@
+package wikiBook2;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class CloseWindowListener extends WindowAdapter {
+
+	WikiBookContainer wikibook;
+	
+    public CloseWindowListener(WikiBookContainer wikibook) {
+    	super();
+    	this.wikibook = wikibook;
+    }
+
+    public void windowClosing(WindowEvent e) {
+        wikibook.getDB().closeConnection();
+    }
+}
---
+++ wikihistorybook/wikiBook2/wikiBook.java
@@ -0,0 +1,10 @@
+package wikiBook2;
+
+public class wikiBook {
+	
+	public static void main(String[] args) {
+		WikiBookContainer test = new WikiBookContainer();
+
+	}
+
+}
